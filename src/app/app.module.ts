b94f1e2b29be1b08c9a9ead1132e2d7ab3095555
thiago quadros pedroso <thiago_q_pedroso@estudante.sesisenai.org.br>
2024-06-25 21:16:29 -0300

    commit inicial + cabecalho + sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { MenuComponent } from './pages/portfolio/menu/menu.component';
+import { CabecalhoComponent } from './pages/portfolio/cabecalho/cabecalho.component';
+import { SobreComponent } from './pages/portfolio/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    MenuComponent,
+    CabecalhoComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
